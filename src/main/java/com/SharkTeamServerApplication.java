7827e2ee4bf5fcb709baa679402ae0a6bf6ebf97
3kybika <vladislav.alehin@mail.ru>
2018-02-27 23:53:34 +0300

    Fixed code style.

--- src/main/java/com/SharkTeamServerApplication.java
+++ src/main/java/com/SharkTeamServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SharkTeamServerApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SharkTeamServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SharkTeamServerApplication.class, args);
+    }
 }
