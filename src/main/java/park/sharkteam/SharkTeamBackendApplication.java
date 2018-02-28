f3cc6fb3ccd91b619f289d6b822f694d8a9ba6a1
3kybika <vladislav.alehin@mail.ru>
2018-03-01 00:51:15 +0300

    Fix codestyles

--- src/main/java/park/sharkteam/SharkTeamBackendApplication.java
+++ src/main/java/park/sharkteam/SharkTeamBackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SharkTeamBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SharkTeamBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+         SpringApplication.run(SharkTeamBackendApplication.class, args);
+    }
 }
