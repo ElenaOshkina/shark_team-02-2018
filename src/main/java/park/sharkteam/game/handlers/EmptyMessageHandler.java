7655fbb66d787f503fca0b8aba05cc549c867eb5
3kybika <vladislav.alehin@mail.ru>
2018-05-17 02:05:28 +0300

    Some message added.

--- src/main/java/park/sharkteam/game/handlers/EmptyMessageHandler.java
+++ src/main/java/park/sharkteam/game/handlers/EmptyMessageHandler.java
@@ -1,17 +1,26 @@
 package park.sharkteam.game.handlers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import park.sharkteam.game.GameSessionOrganizer;
 import park.sharkteam.game.messages.EmptyMessage;
+import park.sharkteam.websocket.GameSocketService;
 import park.sharkteam.websocket.MessageHandler;
 import park.sharkteam.websocket.MessageHandlerContainer;
 
 import javax.validation.constraints.NotNull;
+import java.io.IOException;
 
 @Component
 public class EmptyMessageHandler extends MessageHandler<EmptyMessage> {
 
     @NotNull
     private MessageHandlerContainer messageHandlerContainer;
+
+    @Autowired
+    private GameSocketService gameSocketService;
+    @Autowired
+    private GameSessionOrganizer gameSessionService;
 
     public EmptyMessageHandler(@NotNull MessageHandlerContainer messageHandlerContainer) {
         super(EmptyMessage.class);
@@ -20,6 +29,11 @@ public class EmptyMessageHandler extends MessageHandler<EmptyMessage> {
 
     @Override
     public void handle(@NotNull EmptyMessage message, @NotNull Integer userId) {
-
+        try {
+            gameSocketService.sendMessageToUser(userId, new EmptyMessage());
+        }
+        catch(IOException e){
+            gameSessionService.handleUnexpectedEnding(userId);
+        }
     }
 }
